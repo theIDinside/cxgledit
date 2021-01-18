dbd1e75a129180725b4e417a474463536c6dc13e
Simone Farre <simon.farre.cx@gmail.com>
2021-01-19 00:22:07 +0100

    fixed hilarious bug, provided by Windows being a laughably s#*t OS. If you use <filesystem> and as if a directory exists with, that has the prefix 'con' - you are smoked, the application will crash/throw an exception, because on windows 'con', 'obviously' is an illegal name. So scanning a directory for all files with the prefix 'con' becomes impossible, using the <filesystem> header, since asking the file system directly for 'con' will crash on windows. F#*k me windows is hilariously bad

--- src/core/commands/file_manager.cpp
+++ src/core/commands/file_manager.cpp
@@ -3,15 +3,15 @@
 //
 
 #include "file_manager.hpp"
-#include <ranges>
 #include <core/core.hpp>
+#include <ranges>
 
 FileManager &FileManager::get_instance() {
     static FileManager fm;
     return fm;
 }
 void FileManager::set_user_input(std::string_view v) {
-    if(not v.empty()) {
+    if (not v.empty()) {
         clear_state();
         setup_state();
         prefix = v;
@@ -35,60 +35,53 @@ void FileManager::set_user_input(std::string_view v) {
         if (fs::exists(path) && fs::is_directory(path)) {
             auto begin = fs::directory_iterator(path);
             auto end = fs::end(fs::directory_iterator(path));
-
-            if (fs::is_directory(prefix)) {
-                auto it = std::ranges::subrange(begin, end) | std::views::filter(keepWithPrefix) |
-                          std::views::transform(to_path);
-                std::ranges::copy(it, std::back_inserter(withSamePrefix));
-            } else {
-                auto it = std::ranges::subrange(begin, end) | std::views::filter(keepWithPrefix) |
-                          std::views::transform(to_path);
-                std::ranges::copy(it, std::back_inserter(withSamePrefix));
+            /// NOTE(important): This *have* to be wrapped in a *horrible* if-statement checking for "con"
+            /// because Windows is an absolute shit OS, so trying to check for a directory which has the name/prefix "con"
+            /// *will* crash the standard library, as simply "con" is an illegal name in Windows. so fs::is_directory("con")
+            /// *will* crash the program. Holy shit that Windows is this hilariously bad in 2021.
+            if (filePrefix != "con") {
+                if (fs::is_directory(prefix)) {
+                    auto it = std::ranges::subrange(begin, end) | std::views::filter(keepWithPrefix) |
+                              std::views::transform(to_path);
+                    std::ranges::copy(it, std::back_inserter(withSamePrefix));
+                } else {
+                    auto it = std::ranges::subrange(begin, end) | std::views::filter(keepWithPrefix) |
+                              std::views::transform(to_path);
+                    std::ranges::copy(it, std::back_inserter(withSamePrefix));
+                }
             }
-            // for (const auto &p : withSamePrefix) { fmt::print("{} \t", p.generic_string()); }
-        } else {
-            util::println("PATH DOES NOT EXIST: {}", path.generic_string());
         }
     }
 }
 SelectionResult FileManager::get_suggestion() const {
-    if(not withSamePrefix.empty()) {
-        if(fs::is_directory(withSamePrefix[selected_file])) {
+    if (not withSamePrefix.empty()) {
+        if (fs::is_directory(withSamePrefix[selected_file])) {
             auto path_str = withSamePrefix[selected_file].generic_string();
             path_str.push_back('/');
-            return SelectionResult {
-                    prefix,
-                    path_str
-            };
+            return SelectionResult{prefix, path_str};
         } else {
-            return SelectionResult {
-                    prefix,
-                    withSamePrefix[selected_file].generic_string()
-            };
+            return SelectionResult{prefix, withSamePrefix[selected_file].generic_string()};
         }
     } else {
         return SelectionResult{prefix, {}};
     }
 }
 void FileManager::next_suggestion() {
-    if(not withSamePrefix.empty()) {
+    if (not withSamePrefix.empty()) {
         selected_file++;
         selected_file = selected_file % withSamePrefix.size();
     }
 }
 void FileManager::prev_suggestion() {
-    if(not withSamePrefix.empty()) {
+    if (not withSamePrefix.empty()) {
         selected_file--;
-        if(selected_file < 0) {
-            selected_file = withSamePrefix.size() - 1;
-        }
+        if (selected_file < 0) { selected_file = withSamePrefix.size() - 1; }
     }
 }
 void FileManager::clear_state() {
     prefix.clear();
     withSamePrefix.clear();
     selected_file = 0;
-
 }
 void FileManager::setup_state() {
     current_path = fs::path{fs::current_path().generic_string() + "/"};
